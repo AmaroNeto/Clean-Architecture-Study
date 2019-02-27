60f2e85fe67b2b320ede213607ddd402765a1faa
Amaro Neto <amaro@motorola.com>
2019-02-27 17:12:00 -0300

    criando a interface de repositorio na camada de domain

--- app/src/main/java/com/amaro/todolist/domain/repositories/TodoRepository.kt
+++ app/src/main/java/com/amaro/todolist/domain/repositories/TodoRepository.kt
@@ -1,4 +1,8 @@
 package com.amaro.todolist.domain.repositories
 
+import com.amaro.todolist.domain.entities.TodoDomain
+import io.reactivex.Single
+
 interface TodoRepository {
+    fun getAllTodos() : Single<TodoDomain>
 }
--- app/src/main/java/com/amaro/todolist/domain/usercases/ListTodosUserCase.kt
+++ app/src/main/java/com/amaro/todolist/domain/usercases/ListTodosUserCase.kt
@@ -5,7 +5,7 @@ import com.amaro.todolist.domain.repositories.TodoRepository
 import io.reactivex.Single
 
 class ListTodosUserCase(var todorepository: TodoRepository) : UseCase<TodoDomain, Void>() {
-    override fun execute(params: Void): Single<TodoDomain> {
+    override fun execute(params: TodoDomain): Single<Void> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
