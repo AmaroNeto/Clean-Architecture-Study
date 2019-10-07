ff0993764f2b1c2da2ae211f855ae6f733d4cef3
Amaro Neto <amaro.neto@ifood.com.br>
2019-10-07 18:10:08 -0300

    Koin bug fix

--- app/src/main/java/com/amaro/todolist/di/ViewModel.kt
+++ app/src/main/java/com/amaro/todolist/di/ViewModel.kt
@@ -3,9 +3,10 @@ package com.amaro.todolist.di
 import com.amaro.todolist.presentation.viewmodel.ListTodosViewModel
 import com.amaro.todolist.presentation.viewmodel.NewTodoViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { ListTodosViewModel(get(), get(), get(), get()) }
-    viewModel { NewTodoViewModel(get(), get(), get(), get()) }
+    viewModel { ListTodosViewModel(get(named("ListTodosFlowableObservableUseCase")), get(), get(), get()) }
+    viewModel { NewTodoViewModel(get(named("CreateTodoObservableUseCase")), get(), get(), get()) }
 }
